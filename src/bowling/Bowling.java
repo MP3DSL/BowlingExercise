618ebbbd122eba9cb65cfdd559d689268a988762
Derrick Lemmon <dsl1130@gmail.com>
2020-01-24 11:53:39 -0500

    Can correctly add none strikes and spares, and correctly add strikes and spares up until the 9/10th frames

--- src/bowling/Bowling.java
+++ src/bowling/Bowling.java
@@ -4,17 +4,19 @@ import javax.swing.JOptionPane;
 
 public class Bowling {
 	
-	private static int finalScore = 0;
-
 	public static void main(String[] args) {
-		String scoreSheet = JOptionPane.showInputDialog("Enter Your Scoresheet\n(Separate each frame with hyphens)\nExample \"50-X-X-5/-X-12-36-90-X-X6/\"");
-		String[] dividedScoreSheet = scoreSheet.split("-");
-		for(String score:dividedScoreSheet) {
-			System.out.println(score+"\n");
-			finalScore += addFrame(score);
+		int done = 0;
+		while(done == 0) {
+			String scoreSheet = JOptionPane.showInputDialog("Enter Your Scoresheet\n(Separate each frame with hyphens)\nExample \"50-X-X-5/-X-12-36-90-X-X6/\"");
+			if(scoreSheet == null || scoreSheet.equals(""))
+				System.exit(0);
+			String[] dividedScoreSheet = scoreSheet.split("-");
+			int finalScore = addFrames(dividedScoreSheet);
+			System.out.println("Score Sheet Entered: " + scoreSheet);
+			System.out.println("Final Score: " + finalScore);
+			JOptionPane.showMessageDialog(null, "Final Score: " + finalScore);
+			done = JOptionPane.showConfirmDialog(null, "Would you like to enter another score sheet?");
 		}
-		System.out.println("Score Sheet Entered: " + scoreSheet);
-		System.out.println("Final Score:" + finalScore);
 	}
 	
 	private static int addFrame(String frame) {
@@ -32,6 +34,19 @@ public class Bowling {
 				default:
 					totalScore += Integer.parseInt(score);
 			}
+		}
+		return totalScore;
+	}
+	
+	private static int addFrames(String[] frames) {
+		int totalScore = 0;
+		for(int i = 0; i<frames.length; i++) {
+			if(frames[i].contains("X") && i<8)
+				totalScore += (addFrame(frames[i]) + addFrame(frames[i+1]) + addFrame(frames[i+2]));
+			else if(frames[i].contains("/") && i<9)
+				totalScore += (addFrame(frames[i] + addFrame(frames[i+1])));
+			else
+				totalScore += addFrame(frames[i]);
 		}
 		return totalScore;
 	}
